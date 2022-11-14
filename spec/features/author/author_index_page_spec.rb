6769f07a98147f9766ed8eba19c0fb8c0c46d872
Matvey <Matvey.Sivashinskiy@student.hpi.uni-potsdam.de>
2022-11-14 19:46:21 +0100

    Added Author index page

---
+++ spec/features/author/author_index_page_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Author index page", type: :feature do
+    it "should render withour error" do
+        visit authors_path
+    end
+
+    it "should have a table with the correct headers" do
+        visit authors_path
+        expect(page).to have_table('authors')
+        expect(page).to have_text('Name')
+        expect(page).to have_text('Homepage')
+    end
+
+    #it "should have a show link to the authors individual details page" do
+    #    visit authors_path
+    #    expect(page).to have_link 'Show', href: author_path(@author)
+    #end
+
+    it "should have a link to the new author page" do
+        visit authors_path
+        expect(page).to have_link 'New', href: new_author_path
+    end
+end
