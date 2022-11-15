e20f07496ab5cf9a23d9460d45d21e6ea674e506
Matvey <Matvey.Sivashinskiy@student.hpi.uni-potsdam.de>
2022-11-15 23:10:03 +0100

    Fixed paper author list

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe Paper, type: :model do
   end
 
   it "should not have an empty list of authors" do
-    paper = FactoryBot.create :paper
-    expect(paper.authors).to be_empty
+    empty_paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(empty_paper.authors).to be_empty
   end
 end
