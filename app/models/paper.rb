7d818abed1b053b9ca2c21dafa7e831f47e3857c
Matvey <Matvey.Sivashinskiy@student.hpi.uni-potsdam.de>
2022-11-15 20:16:06 +0100

    Added paper validation

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,5 @@
 class Paper < ApplicationRecord
+    validates :title, presence: true, allow_blank: false
+    validates :venue, presence: true, allow_blank: false
+    validates :year, presence: true, allow_blank: false, numericality: { only_integer: true }
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should validate with all attributes" do 
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(paper).to be_valid
+  end 
+
+  it "should not validate without title" do 
+    paper = Paper.new(venue: "Mind 49: 433-460", year: 1950)
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate without venue" do 
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", year: 1950)
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate without year" do 
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460")
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate with non-integer year" do 
+    paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: "nineteen fifty")
+    expect(paper).to_not be_valid
+  end
 end
